12a67329e42e6dd37fc35ba74e392f6fe1b2c675
pratiksha317 <64767578+pratiksha317@users.noreply.github.com>
2020-05-04 14:19:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+// app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+    }
+    next();
+  });
+
 //Connect DB
 connectDB();
 
